7a8fd0aa15c6a3372e7dc2b8140196e6cbdf8d4f
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-08-20 12:13:24 +0100

    remove NVIDIA repo as not needed

--- files/scripts/build-system.sh
+++ files/scripts/build-system.sh
@@ -3,5 +3,10 @@
 # Enable terra respository - disabled as bazzite has this already
 #dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 # Configure terra repositories
-dnf5 -y install terra-release-mesa terra-release-nvidia terra-release-extras
+dnf5 -y install terra-release-mesa terra-release-extras
 dn5 -y install winetricks vlc localsend-bin nvidia-patch heroic-games-launcher faugus-launcher protonplus gpu-screen-recorder gpu-screen-recorder-ui steamtinkerlaunch
+
+# Install github-cli
+dnf5 -y install gh
+
+# Install custom-built
