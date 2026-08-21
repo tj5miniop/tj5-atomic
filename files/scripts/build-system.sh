c77e3378391ab7c2e587337f7ba70f2d1a553f74
tj5miniop <82961157+tj5miniop@users.noreply.github.com>
2026-08-21 22:24:15 +0100

    add more flatpaks, switch browser to ZEN and fix typo

--- files/scripts/build-system.sh
+++ files/scripts/build-system.sh
@@ -1,12 +1,16 @@
 #!/bin/bash 
-
 # Enable terra respository - disabled as bazzite has this already
 #dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 # Configure terra repositories
 dnf5 -y install terra-release-mesa terra-release-extras
-dn5 -y install winetricks vlc localsend-bin nvidia-patch heroic-games-launcher faugus-launcher protonplus gpu-screen-recorder gpu-screen-recorder-ui steamtinkerlaunch
+dnf5 -y install winetricks vlc localsend-bin nvidia-patch heroic-games-launcher faugus-launcher protonplus gpu-screen-recorder gpu-screen-recorder-ui steamtinkerlaunch
 
 # Install github-cli
 dnf5 -y install gh
 
-# Install custom-built
+# Remove certain bazzite packages to help prevent some issues 
+dnf5 -y remove lutris 
+
+# Install and add certain applications which is not usually included inside bazzite by default
+dnf5 -y install sunshine ds-inhibit ryzen-adj
+
